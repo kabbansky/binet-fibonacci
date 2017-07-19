f268dc9733cbce14db9114c6cfa667c56ca9ae38
kabbansky <san4ell@gmail.com>
2017-07-19 13:24:41 +0300

    some PEP-ish indentation, thanks @sodrian

--- binet-fibonacci.py
+++ binet-fibonacci.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python
-import flask
-from flask import Flask, request, Response
 import sys, math, decimal
+from flask import Flask, request, Response
 from math import sqrt
 from decimal import Decimal
 
+
 app = Flask(__name__)
+
 
 @app.route('/', methods=['GET'])
 def api_help():
     return app.send_static_file('help.html')
 
+
 @app.route('/api/<int:n>', methods=['GET'])
 def main(n):
-    
     #go for some higher precision with Decimal
     gr = Decimal((1 + sqrt(5)) / 2 )
     div = Decimal(2 * gr - 1 )
@@ -29,6 +30,7 @@ def main(n):
         sys.exit(0)
     else:
         return str("%d" % ln)
+
 
 if __name__ == "__main__":
     app.run()
